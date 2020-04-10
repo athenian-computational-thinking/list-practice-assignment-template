45dbd9a35c884f33b3cd8a0f871cb4e024fe7bb9
Megan Leich <mleich@athenian.org>
2020-04-10 03:01:07 +0000

    edited starter code

--- my_code.py
+++ my_code.py
@@ -1,38 +1,34 @@
 # Complete the functions below to manipulate the given list. 
 # Be sure to follow the directions carefully to be sure your results match the expected results.
 
-def append_to_list(x): 
+def append_to_list():      #Be sure to choose the parameter for your function
     # User code goes here
-    x.append(123)
-    return x
+    return                  # Return your new list
 
-def insert_to_list(y):
-    y.insert(2,12)
-    return y
+def insert_to_list():      #Complete this function to insert 12 in the [2] position and return the new list.
+    
+    return                 
 
-def remove_from_list(a):
-    a.remove(8)
-    return a
+def remove_from_list():   # Complete this function to remove 8 from the list and return the new list.
+    
+    return 
 
-def sort_ascending(z):
-    z.sort()
-    return z
+def sort_ascending():     #Complete this function to sort the list from low to high.
+    
+    return 
 
-def check_list(d):
-    if 33 in d:
-        check = True
-    else:
-        check = False
-    return check
+def check_list():        #Complete this function to check if 33 is in the list. Return True if it is and False if it is not.
+    
+    return 
 
 if __name__ == '__main__':
     # Use the print statements below to check your code. Do not change starter_list or your program will not pass automated test.
     starter_list = [3, 18, 2, 75, 8, 33]
     
-    print(append_to_list(starter_list))
-    print(insert_to_list(starter_list))
-    print(remove_from_list(starter_list))
-    print(sort_ascending(starter_list))
-    print(check_list(starter_list))
+    #print(append_to_list(starter_list))
+    #print(insert_to_list(starter_list))
+    #print(remove_from_list(starter_list))
+    #print(sort_ascending(starter_list))
+    #print(check_list(starter_list))
 
 
