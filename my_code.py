5476161650a4904af6f2fdc4e3946d90a3d3b23a
Megan Leich <mleich@athenian.org>
2020-04-10 02:52:32 +0000

    added test code and started starter code

--- my_code.py
+++ my_code.py
@@ -1,13 +1,38 @@
-def inc(x):
-    # User code goes here
-    return x + 1
+# Complete the functions below to manipulate the given list. 
+# Be sure to follow the directions carefully to be sure your results match the expected results.
 
+def append_to_list(x): 
+    # User code goes here
+    x.append(123)
+    return x
+
+def insert_to_list(y):
+    y.insert(2,12)
+    return y
+
+def remove_from_list(a):
+    a.remove(8)
+    return a
+
+def sort_ascending(z):
+    z.sort()
+    return z
+
+def check_list(d):
+    if 33 in d:
+        check = True
+    else:
+        check = False
+    return check
 
 if __name__ == '__main__':
-    # Test your code with this first
-    # Change the argument to try different values
-    print(inc(6))
+    # Use the print statements below to check your code. Do not change starter_list or your program will not pass automated test.
+    starter_list = [3, 18, 2, 75, 8, 33]
+    
+    print(append_to_list(starter_list))
+    print(insert_to_list(starter_list))
+    print(remove_from_list(starter_list))
+    print(sort_ascending(starter_list))
+    print(check_list(starter_list))
 
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #v = input("Value to increment: ")
-    #print(inc(int(v)))
+
--- my_test.py
+++ my_test.py
@@ -1,7 +1,26 @@
-from my_code import inc
+from my_code import append_to_list
+from my_code import insert_to_list
+from my_code import remove_from_list
+from my_code import sort_ascending
+from my_code import check_list
 
 
-def test_inc():
-    assert 5 == inc(4)
-    assert 1 == inc(0)
-    assert 0 == inc(-1)
+list = [3, 18, 2, 75, 8, 33]
+
+def test_append_to_list():
+    assert [3, 18, 2, 75, 8, 33, 123] == append_to_list(list)
+
+def test_insert_to_list():
+    assert [3, 18, 12, 2, 75, 8, 33, 123] == insert_to_list(list)
+
+
+def test_remove_from_list():
+    assert [3, 18, 12, 2, 75, 33, 123] == remove_from_list(list)
+
+
+def test_sort_ascending():
+    assert [2, 3, 12, 18, 33, 75, 123] == sort_ascending(list)
+
+
+def test_check_list():
+    assert True == check_list(list) 
